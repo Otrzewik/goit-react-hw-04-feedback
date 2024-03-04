1f1eae767dea5692346dedac3f3c53327c23172c
Otrzewik <trzewik.ola.aleksandra@gmail.com>
2024-03-04 20:14:56 +0100

    finalupdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
-import { Section } from "./Section/Section";
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Statistics } from "./Statistics/Statistics";
-import css from "./App.module.css";
-
+import { useState } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import css from './App.module.css';
 
 export const App = () => {
   const [good, setGood] = useState(0);
@@ -11,26 +10,29 @@ export const App = () => {
   const [bad, setBad] = useState(0);
   const [totalFeedback, setTotalFeedback] = useState(0);
 
-  const onLeaveFeedback = (event) => {
+  const onLeaveFeedback = event => {
     switch (event.target.id) {
-      case "Good":
-        setGood((prev) => prev + 1);
+      case 'Good':
+        setGood(prev => prev + 1);
         break;
-      case "Neutral":
-        setNeutral((prev) => prev + 1);
+      case 'Neutral':
+        setNeutral(prev => prev + 1);
         break;
-      case "Bad":
-        setBad((prev) => prev + 1);
+      case 'Bad':
+        setBad(prev => prev + 1);
         break;
       default:
     }
-    setTotalFeedback((prev) => prev + 1);
-  }
+    setTotalFeedback(prev => prev + 1);
+  };
 
   return (
     <div className={css.feedback}>
       <Section title="Please leave feedback">
-        <FeedbackOptions options={["Good", "Neutral", "Bad"]} onLeaveFeedback={onLeaveFeedback} />
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
       </Section>
       <Section title="Statistics">
         <Statistics
